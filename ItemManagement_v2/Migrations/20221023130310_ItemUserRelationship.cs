cb880f4481c03fea2af3c82344490f080b3d883d
Szczepan <szczep.kowalik@gmail.com>
2022-10-24 17:13:51 +0200

    Models and migrations

---
+++ ItemManagement_v2/Migrations/20221023130310_ItemUserRelationship.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItemManagement_v2.Migrations
+{
+    public partial class ItemUserRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Items",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_IdentityUserId",
+                table: "Items",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_AspNetUsers_IdentityUserId",
+                table: "Items",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_AspNetUsers_IdentityUserId",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_IdentityUserId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Items");
+        }
+    }
+}
---
+++ ItemManagement_v2/Models/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ItemManagement_v2.Models
+{
+    public class Collection
+    {
+       
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
